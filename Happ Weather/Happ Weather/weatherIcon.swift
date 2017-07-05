5ae65d474c5b0b9eec7ca16e6be57f46113e048e
Your Name <rintudhaya@gmail.com>
2017-07-05 17:27:30 -0400

    added some code snippets

--- Happ Weather/Happ Weather/weatherIcon.swift
+++ Happ Weather/Happ Weather/weatherIcon.swift
@@ -38,23 +38,23 @@ enum WeatherIcons {
         }
 }
 }
-//extension WeatherIcons {
-//    var image: UIImage {
-//        switch self {
-//        case .clearday: return #imageLiteral(resourceName: "clear-day.png")
-//        case .clearnight: return  #imageLiteral(resourceName: "clear-night.png")
-//        case .rain: return #imageLiteral(resourceName: "rain.png")
-//        case .sleet: return #imageLiteral(resourceName: "sleet.png")
-//        case .snow: return #imageLiteral(resourceName: "snow.png")
-//        case .wind: return #imageLiteral(resourceName: "wind.png")
-//        case .fog: return #imageLiteral(resourceName: "fog.png")
-//        case .cloudy : return #imageLiteral(resourceName: "cloud.png")
-//        case .partlyCloudyDay: return #imageLiteral(resourceName: "cloudy-day.png")
-//        case .partlyCloudyNight: return #imageLiteral(resourceName: "cloudy-night.png")
-//        case default: return `default`
-//        }
-//    }
-//}
+extension WeatherIcons {
+    var image: UIImage {
+        switch self {
+        case .clearday: return #imageLiteral(resourceName: "clear-day.png")
+        case .clearnight: return  #imageLiteral(resourceName: "clear-night.png")
+        case .rain: return #imageLiteral(resourceName: "rain.png")
+        case .sleet: return #imageLiteral(resourceName: "sleet.png")
+        case .snow: return #imageLiteral(resourceName: "snow.png")
+        case .wind: return #imageLiteral(resourceName: "wind.png")
+        case .fog: return #imageLiteral(resourceName: "fog.png")
+        case .cloudy : return #imageLiteral(resourceName: "cloud.png")
+        case .partlyCloudyDay: return #imageLiteral(resourceName: "cloudy-day.png")
+        case .partlyCloudyNight: return #imageLiteral(resourceName: "cloudy-night.png")
+        case .default: return #imageLiteral(resourceName: "default.png")
+        }
+    }
+}
 
 
 
